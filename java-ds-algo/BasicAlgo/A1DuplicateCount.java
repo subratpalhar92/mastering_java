6750f1be4fc75aeff9625d566a17a53106edd555
subratpalhar92 <subratpalhar92@gmail.com>
2025-05-06 08:52:52 +0530

    refactor duplicate count algorithm

--- java-ds-algo/BasicAlgo/A1DuplicateCount.java
+++ java-ds-algo/BasicAlgo/A1DuplicateCount.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -10,9 +12,14 @@ public class A1DuplicateCount {
     static List<Integer> myList = Arrays.asList(2,5,3,4,4,7,7,9,3,3);
 
     public static void main(String args[]) {
+
+        /**
+         * Using Iterator
+         */
         Map<Integer, Integer> mp = new HashMap<>();
-        Integer []arr = myList.toArray(new Integer[0]); // Object
-        for (int x : arr) {
+        Iterator<Integer> it = myList.iterator();
+        while (it.hasNext()) {
+            int x = it.next();
             if (mp.containsKey(x)) {
                 mp.put(x, (mp.get(x) + 1));
             } else {
@@ -21,10 +28,28 @@ public class A1DuplicateCount {
         }
         System.out.println(mp);
 
+        /**
+         * Using Array
+         */
+        Map<Integer, Integer> hmp = new HashMap<>();
+        Integer []arr = myList.toArray(new Integer[0]); // Object
+        for (int x : arr) {
+            if (hmp.containsKey(x)) {
+                hmp.put(x, (hmp.get(x) + 1));
+            } else {
+                hmp.put(x, 1);
+            }
+        }
+        System.out.println(hmp);
 
+
+        /**
+         * Using Stream API
+         */
         Map<Integer, Long> duplicateCount = myList.stream().collect(
             Collectors.groupingBy(Function.identity(), Collectors.counting())
         );
         System.out.println(duplicateCount);
     }
+
 }
